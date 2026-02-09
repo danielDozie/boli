828144c6cfe9da2a8cd1621b20ca8cfa1c629138
danielDozie <placmedia@gmail.com>
2026-02-09 02:39:35 +0100

    feat: switch Astro adapter from Node.js to Vercel and update related configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import react from '@astrojs/react';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
 });
